37e829a7db84def4275a7a1e9aad3f0f1c34369d
alisonys <452592978@qq.com>
2019-01-11 09:37:20 +0800

    add if else statement

--- demo.js
+++ demo.js
@@ -4,13 +4,19 @@ var fs = require('fs')
 
 var dirName = process.argv[2]
 
-fs.mkdirSync("./" + dirName)
-process.chdir("./" + dirName)
-fs.mkdirSync("./css")
-fs.mkdirSync("js")
+if (typeof dirName === "undefined") {
+  fs.mkdirSync("./" + dirName);
+  process.chdir("./" + dirName);
+  fs.mkdirSync("./css");
+  fs.mkdirSync("js");
+  
+  fs.writeFileSync("index.html","<!DOCTYPE>  <title>Hello</title>  <h1>Hi</h1>");
+  fs.writeFileSync("./css/style.css","h1{color: red;}");
+  fs.writeFileSync("js/main.js","var string = 'Hello World';   alert(string)");
+  
+  process.exit(0);
+}else {
+  alert("faied, because dir exists");
+  process.exit(1);
+}
 
-fs.writeFileSync("index.html","<!DOCTYPE>  <title>Hello</title>  <h1>Hi</h1>")
-fs.writeFileSync("./css/style.css","h1{color: red;}")
-fs.writeFileSync("js/main.js","var string = 'Hello World';   alert(string)")
-
-process.exit(0)
